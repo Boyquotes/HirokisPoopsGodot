a58fb2b20d38861da4655857525c396badb94f12
mak448a <mak448a@gmail.com>
2023-01-23 23:37:56 -0600

    Add score

--- src/LevelBoilerPlate.gd
+++ src/LevelBoilerPlate.gd
@@ -3,10 +3,17 @@ extends Node2D
 export (PackedScene) var poop_scene
 export (PackedScene) var cloud_scene
 
+var score: float = 0.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	# Every single call, we need to add by a certain amount.
+	score += delta * 20
+	$UI/ScoreLayer/Score.text = str(int(score))
 
 
 func _on_Poo_Timer_timeout() -> void:
