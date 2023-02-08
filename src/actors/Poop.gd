f9ac3eccce6689da5d2b1df9101ee4f249f3b238
mak448a <mak448a@gmail.com>
2023-02-07 22:02:34 -0600

    Fix wording

--- src/actors/Poop.gd
+++ src/actors/Poop.gd
@@ -7,7 +7,7 @@ var velocity := Vector2(0, 0)
 var generator := RandomNumberGenerator.new()
 var speed := 0
 export (PackedScene) var explosion_scene
-var exploded = true
+var not_exploded = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,7 +23,7 @@ func _process(delta):
 		queue_free()
 
 func _physics_process(delta):
-	if exploded:
+	if not_exploded:
 		var direction = Vector2.ZERO
 		
 		direction.x -= 1
@@ -39,11 +39,11 @@ func _on_Poop_body_entered(body):
 
 
 func explode(bullet):
-	if exploded:
+	if not_exploded:
 		var explosion = explosion_scene.instance()
 		$Explosions.add_child(explosion)
 		get_node(".").monitoring = false
 		$Poop.visible = false
-		exploded = false
+		not_exploded = false
 		bullet.queue_free()
 		globals.score += 20
