d14c41b164712a4d5ad742d46bf1984967fe6fec
mak448a <mak448a@gmail.com>
2023-02-10 16:54:27 -0600

    Remove print statement

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -35,7 +35,6 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
-		print(direction.x * speed * delta)
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("ui_down"):
