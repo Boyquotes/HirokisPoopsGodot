957f0a48b40c3887ae0ae3689e1461f58b23c334
mak448a <mak448a@gmail.com>
2023-02-11 21:50:07 -0600

    comment out print statement

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -20,10 +20,11 @@ func _ready():
 	globals.bullets = 10
 	
 	if OS.get_name() == "HTML5":
-		print("Override FPS to be 30!!")
+		#print("Override FPS to be 30!!")
 		Engine.set_target_fps(30)
 	else:
-		print("60 FPS")
+		pass
+		#print("60 FPS")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
