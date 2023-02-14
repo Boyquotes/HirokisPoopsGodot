129b8efb9be35fcfb56a254f2674b93df008ef52
mak448a <mak448a@gmail.com>
2023-02-13 18:49:49 -0600

    Add keyboard support to credits

--- src/menus/Credits.gd
+++ src/menus/Credits.gd
@@ -5,6 +5,11 @@ extends Control
 func _ready() -> void:
 	$"Back".grab_focus()
 
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_down"):
+		$"%ScrollContainer".scroll_vertical += 8
+	if Input.is_action_pressed("ui_up"):
+		$"%ScrollContainer".scroll_vertical -= 8
 
 func _on_Back_pressed() -> void:
 	get_tree().change_scene("res://src/menus/Main.tscn")
