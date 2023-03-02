787a979329b3411067987c5f8d0901ffcccd8e4e
mak448a <mak448a@gmail.com>
2023-03-01 23:32:41 -0600

    Revert "Fullscreen when running from the editor"
    This reverts commit d4834e5260e3f8668ad35ea71e42ae0f9f7647aa.

--- src/globals.gd
+++ src/globals.gd
@@ -8,9 +8,6 @@ var crashed: bool = false
 
 func _ready() -> void:
 	self.pause_mode = Node.PAUSE_MODE_PROCESS
-	# Fullscreen in editor
-	if not OS.has_feature("standalone"):
-		OS.set_window_fullscreen(true)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("fullscreen"):
