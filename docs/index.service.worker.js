32f35208a65afaf9f9ab055c5172d3af73d380db
mak448a <mak448a@gmail.com>
2023-03-27 20:16:13 -0500

    Update web version

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1677789331|563174614";
+const CACHE_VERSION = "1679966143|308370063";
 const CACHE_PREFIX = "Hiroki's Poops-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
