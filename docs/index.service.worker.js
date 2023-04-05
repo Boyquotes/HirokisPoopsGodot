c409f0974637a0091e63cf448986c45b72c38bde
mak448a <mak448a@gmail.com>
2023-04-05 09:47:45 -0500

    Update mobile icon

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1679966143|308370063";
+const CACHE_VERSION = "1680705961|616246569";
 const CACHE_PREFIX = "Hiroki's Poops-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
