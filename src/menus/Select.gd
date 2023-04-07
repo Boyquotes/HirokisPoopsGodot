49f3a423c3a3044962f004b4de8da1e23aa67637
mak448a <mak448a@gmail.com>
2023-04-06 22:44:10 -0500

    Add connah

--- src/menus/Select.gd
+++ src/menus/Select.gd
@@ -45,9 +45,16 @@ func _on_Mario_pressed() -> void:
 		yield(get_tree().create_timer(.2), "timeout")
 	get_tree().change_scene("res://src/Mario.tscn")
 
+func _on_Conner_pressed() -> void:
+	if OS.get_name() == "HTML5" or OS.get_name() == "Android":
+		yield(get_tree().create_timer(.2), "timeout")
+	get_tree().change_scene("res://src/Conner.tscn")
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_pressed("ui_right") and current == "e":
 		activate_mario()
+	if Input.is_action_pressed("ui_left") and current == "h":
+		activate_conner()
 
 
 func activate_mario():
@@ -57,6 +64,17 @@ func activate_mario():
 		$"%Mario".grab_focus()
 		$MainUI/Portrait.texture = load("res://assets/img/portraits/mario-portrait.png")
 
+func activate_conner():
+	if not $"%Conner".visible:
+		$"%ConnerSound".play()
+		$"%Conner".visible = true
+		$"%Conner".grab_focus()
+		$MainUI/Portrait.texture = load("res://assets/img/portraits/conner-portrait.png")
+
 func _on_Mario_entered() -> void:
 	$"%Mario".grab_focus()
 	$MainUI/Portrait.texture = load("res://assets/img/portraits/mario-portrait.png")
+
+func _on_Conner_entered() -> void:
+	$"%Conner".grab_focus()
+	$MainUI/Portrait.texture = load("res://assets/img/portraits/conner-portrait.png")
